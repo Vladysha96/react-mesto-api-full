f72bdb9d21a88af80e576440ef8333b4184b5e51
Vlad Kartashov <kazus92@yandex.ru>
2022-11-21 14:13:33 +0300

    add front & back

--- backend/app.js
+++ backend/app.js
@@ -19,52 +19,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://vladysha96.frontend.nomoredomains.icu/',
-    'http://vladysha96.frontend.nomoredomains.icu/',
-    'https://vladysha96.backend.nomoredomains.icu/',
-    'http://vladysha96.backend.nomoredomains.icu/',
-  ],
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    'Access-Control-Allow-Methods',
-    'Access-Control-Request-Headers',
-    'Access-Control-Allow-Origin',
-  ],
-  credentials: true,
-  enablePreflight: true,
-}));
-
-const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'https://vladysha96.frontend.nomoredomains.icu/',
-  'http://vladysha96.frontend.nomoredomains.icu/',
-  'https://vladysha96.backend.nomoredomains.icu/',
-  'http://vladysha96.backend.nomoredomains.icu/',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Credentials', true);
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-});
+app.use(cors());
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
